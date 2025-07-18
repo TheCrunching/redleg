8409ee7c73b66c9b06017cec4685ed92def9242c
TheCrunching <theCrunching@proton.me>
2025-07-18 08:06:27 -0400

    modified:   src/redleg/__main__.py

--- src/redleg/__main__.py
+++ src/redleg/__main__.py
@@ -5,13 +5,29 @@ import sys
 import logging
 import argparse
 from .ui import UI
+from .tui import TUI
 
-__version__ = "1.1.0-dev0"
+__version__ = "1.1.0-dev1"
 
 # ? Constants
 # The format for the logging msg
 LOGGING_FORMAT = "[%(levelname)s]: %(message)s"
 
+# Get the logger for the module
+logger = logging.getLogger(__name__)
+# Set up the formatter object
+formatter = logging.Formatter(fmt=LOGGING_FORMAT)
+# Set up console handler
+streamHandler = logging.StreamHandler()
+# Set the formatter for the console handler
+streamHandler.setFormatter(formatter)
+# Add the handler to logger
+logger.addHandler(streamHandler)
+# Set logging level
+if __debug__:
+    logger.setLevel(logging.DEBUG)
+else:
+    logger.setLevel(logging.info)
 
 # Set up argument parser with the program details
 parser = argparse.ArgumentParser(
@@ -30,61 +46,54 @@ parser.add_argument(
     "file",
     help="The ledger file"
 )
-# Set up subparser
-subparsers = parser.add_subparsers(
-    required=True,
-    dest="command"  # Put this here so we know what command is called
-)
-# Subparser for register command
-registerCommand = subparsers.add_parser(
-    "register",
-    help="Prints the register"
-)
-# Subparser for transaction command
-transactionCommand = subparsers.add_parser(
-    "transaction",
-    help="Make a transaction"
-)
-# Subparser for account command
-accountCommand = subparsers.add_parser(
-    "accounts",
-    help="Print the value of all accounts"
-)
-# Subparser for statement command
-statementCommand = subparsers.add_parser(
-    "statement",
-    help="Prints out a statement"
-)
 
-# If no arguments are passed
-if len(sys.argv) == 1:  # Set to 1 cause sys.argv[0] is the program name
-    parser.print_help()
-    sys.exit(2)  # Exit 2 cause of argparse error
-else:  # Else parser the  args
-    args = parser.parse_args()
+def subparsers():
+    # Set up subparser
+    subparsers = parser.add_subparsers(
+        required=True,
+        dest="command"  # Put this here so we know what command is called
+    )
+    # Subparser for register command
+    subparsers.add_parser(
+        "register",
+        help="Prints the register"
+    )
+    # Subparser for transaction command
+    subparsers.add_parser(
+        "transaction",
+        help="Make a transaction"
+    )
+    # Subparser for account command
+    subparsers.add_parser(
+        "accounts",
+        help="Print the value of all accounts"
+    )
+    # Subparser for statement command
+    subparsers.add_parser(
+        "statement",
+        help="Prints out a statement"
+    )
 
-# Get the logger for the module
-logger = logging.getLogger(__name__)
-# Set up the formatter object
-formatter = logging.Formatter(fmt=LOGGING_FORMAT)
-# Set up console handler
-streamHandler = logging.StreamHandler()
-# Set the formatter for the console handler
-streamHandler.setFormatter(formatter)
-# Add the handler to logger
-logger.addHandler(streamHandler)
-# Set logging level
-if __debug__:
-    logger.setLevel(logging.DEBUG)
-else:
-    logger.setLevel(logging.info)
+
+def parse_args():
+    """Parses the command arguments"""
+    # If no arguments are passed
+    if len(sys.argv) == 1:  # Set to 1 cause sys.argv[0] is the program name
+        parser.print_help()
+        sys.exit(2)  # Exit 2 cause of argparse error
+    else:  # Else parser the  args
+        return parser.parse_args()
 
 
 def main() -> int:
     """Handles text user interface"""
+
+    subparsers()
+    args =parse_args()
+
     try:
-        ui = UI(args.file)
-        ui.command(args.command)
+        with UI(args.file) as ui:
+            ui.command(args.command)
     # In case ctrl+c is pressed
     except KeyboardInterrupt:
         logger.error("\nCtrl+C pressed, quitting...")
@@ -93,15 +102,26 @@ def main() -> int:
     except KeyError as e:
         logger.critical("Key: '%s' was missing from ledger file.", e)
         return 1  # Error
-    finally:
-        exit_code = ui.done()
 
-    return exit_code  # Success
+    return 0  # Success
 
 
-def tui() -> int:
+def main_tui() -> int:
     """Handles curses code"""
-    pass
+    args = parse_args()
+    try:
+        with TUI(args.file) as tui:
+            tui.command()
+    # In case ctrl+c is pressed
+    except KeyboardInterrupt:
+        logger.error("\nCtrl+C pressed, quitting...")
+        return 3221225786  # Ctrl+c exit code
+    # In case they tamper with the ledger file and we get a key error
+    except KeyError as e:
+        logger.critical("Key: '%s' was missing from ledger file.", e)
+        return 1  # Error
+
+    return 0  # Success
 
 
 # In case we are executing this script by itself
