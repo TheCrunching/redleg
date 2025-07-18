605f3a0b7f52d7d28e12f9abd04f3da8e3e04a90
TheCrunching <theCrunching@proton.me>
2025-07-18 12:32:45 -0400

    modified:   src/redleg/tui.py

--- src/redleg/tui.py
+++ src/redleg/tui.py
@@ -9,8 +9,9 @@ from .ledger import LedgerFile, LedgerCommands
 logger = logging.getLogger(__name__)
 
 
-
 class Menu():
+    """A menu thingy"""
+    # https://stackoverflow.com/questions/14200721/how-to-create-a-menu-and-submenus-in-python-curses#14205494
     def __init__(self, items, stdscreen):
         self.window = stdscreen.subwin(0, 0)
         self.window.keypad(1)
@@ -23,6 +24,7 @@ class Menu():
         self.items.append(("exit", "exit"))
 
     def navigate(self, n):
+        """Navigates the menu"""
         self.position += n
         if self.position < 0:
             self.position = 0
@@ -30,6 +32,7 @@ class Menu():
             self.position = len(self.items) - 1
 
     def display(self):
+        """Show the panel"""
         self.panel.top()
         self.panel.show()
         self.window.clear()
@@ -43,7 +46,7 @@ class Menu():
                 else:
                     mode = curses.A_NORMAL
 
-                msg = "%d. %s" % (index, item[0])
+                msg = f"{index} {item[0]}"
                 self.window.addstr(1 + index, 1, msg, mode)
 
             key = self.window.getch()
@@ -69,7 +72,8 @@ class TUI():
     """TUI code"""
     def __init__(self, file):
         self.stdscr = None
-        with LedgerFile(file, mode="r") as ledger_file:
+        self.file = file
+        with LedgerFile(self.file, mode="r") as ledger_file:
             self.account_data = ledger_file.read()
             self.commands = LedgerCommands(self.account_data)
 
@@ -99,62 +103,58 @@ class TUI():
         menu = Menu(items, self.stdscr)
         menu.display()
 
-        # Command transaction
-        if command == "transaction":
-            # Get the date
-            date = input("Date (YY-mm-dd): ")
-            description = input("Input transaction description: ")
-            get_accounts = True
-            accounts = {}
-            while get_accounts:
-                # Get account
-                name = input("Account name: ")
-                # If enter pressed then break
-                if name == "":
-                    get_accounts = False
-                    break
-                # Make sure account not already used during this transaction
-                if name in accounts:
-                    raise ValueError("You have input an account twice")
-                while True:
-                    try:
-                        accounts[name] = int(input("Amount: "))
-                        break
-                    except ValueError:
-                        logger.warning("You inputted a non int as a number.")
-                        continue
-            commands.transaction_command(
-                date=date,
-                description=description,
-                accounts=accounts,
-                file=self.file
-            )
-        elif command == "statement":
-            period = input(
-                "Please input a month or a year (YY-mm or YY): "
-            )
-            statement = commands.statement_command(period)
-            print(statement)
-            save_to_file = input("Save to file? [Y/n]: ").lower()
-            if save_to_file in ("y", ""):
-                with open(
-                    f"statement-{period}.txt", mode="w", encoding="utf-8"
-                ) as statement_file:
-                    statement_file.write(statement)
-                print(f"Statement saved: 'statement-{period}.txt'")
-            else:
-                print("Not saving to file")
-        else:
-            print(f"Invalid command passed: '{command}'")
-
     def register_command(self):
+        """Prints all transactions"""
         print(self.commands.register_command())
 
     def transaction_command(self):
-        pass
+        """Makes a transaction"""
+        # Get the date
+        date = input("Date (YY-mm-dd): ")
+        description = input("Input transaction description: ")
+        get_accounts = True
+        accounts = {}
+        while get_accounts:
+            # Get account
+            name = input("Account name: ")
+            # If enter pressed then break
+            if name == "":
+                get_accounts = False
+                break
+            # Make sure account not already used during this transaction
+            if name in accounts:
+                raise ValueError("You have input an account twice")
+            while True:
+                try:
+                    accounts[name] = int(input("Amount: "))
+                    break
+                except ValueError:
+                    logger.warning("You inputted a non int as a number.")
+                    continue
+        self.commands.transaction_command(
+            date=date,
+            description=description,
+            accounts=accounts,
+            file=self.file
+        )
 
-    def  accounts_command(self):
+    def accounts_command(self):
+        """Prints account balances"""
         print(self.commands.account_balance_command())
 
     def statement_command(self):
-        pass
+        """Makes a statement"""
+        period = input(
+            "Please input a month or a year (YY-mm or YY): "
+        )
+        statement = self.commands.statement_command(period)
+        print(statement)
+        save_to_file = input("Save to file? [Y/n]: ").lower()
+        if save_to_file in ("y", ""):
+            with open(
+                f"statement-{period}.txt", mode="w", encoding="utf-8"
+            ) as statement_file:
+                statement_file.write(statement)
+            print(f"Statement saved: 'statement-{period}.txt'")
+        else:
+            print("Not saving to file")
